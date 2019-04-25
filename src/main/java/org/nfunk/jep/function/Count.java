a1897d8e424f0e835816da7c148e59fdf31e2e7b
张毅 <zhangyi183790@hollysys.net>
2019-04-25 12:40:46 +0800

    v2.5.0

---
+++ src/main/java/org/nfunk/jep/function/Count.java
@@ -0,0 +1,45 @@
+package org.nfunk.jep.function;
+
+import java.util.Stack;
+
+import org.nfunk.jep.ParseException;
+
+/**
+ * This class serves mainly as an example of a function that accepts any number
+ * of parameters. Note that the numberOfParameters is initialized to -1.
+ */
+@SuppressWarnings("all")
+public class Count extends PostfixMathCommand {
+
+	/**
+	 * Constructor.
+	 */
+	public Count() {
+		// Use a variable number of arguments
+		numberOfParameters = -1;
+	}
+
+	/**
+	 * Calculates the result of summing up all parameters, which are assumed to
+	 * be of the Double type.
+	 */
+	public void run(Stack stack) throws ParseException {
+		checkStack(stack);// check the stack
+		if (curNumberOfParameters < 1)
+			throw new ParseException("No arguments for Max");
+		int i = 0;
+		// repeat summation for each one of the current parameters
+		while (i < curNumberOfParameters) {
+			// get the parameter from the stack
+			Object param = stack.pop();
+			// it to the max (order is important for String arguments)
+			if (param == null) {
+				continue;
+// 				throw new ParseException("Invalid parameter type");
+			}
+			i++;
+		}
+		// push the result on the inStack
+		stack.push(i);
+	}
+}
